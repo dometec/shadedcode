e5ff0bc2330e624088131bcd6b4d8a882ab6c9ea
dometec <dometec@gmail.com>
2012-05-23 14:35:39 +0200

    esempio di HTTP 304

--- demo-webapp/src/main/java/org/example/demo/controller/Presentation.java
+++ demo-webapp/src/main/java/org/example/demo/controller/Presentation.java
@@ -2,12 +2,17 @@ package org.example.demo.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import com.sun.jersey.spi.resource.PerRequest;
@@ -16,10 +21,14 @@ import com.sun.jersey.spi.resource.PerRequest;
 @Path("services/JugMilano/People/DomenicoBriganti")
 public class Presentation {
 
+	@Context
+	Request request;
+
 	private UserDetails userdet;
 
 	public Presentation() {
 		userdet = new UserDetails();
+		userdet.setVersion(1);
 		userdet.setCompany("Eidon srl");
 		userdet.setEmail("dometec@gmail.com");
 		userdet.setLinkedin("http://www.linkedin.com/in/dometec");
@@ -29,14 +38,28 @@ public class Presentation {
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	public Response getUserDetails() {
-		return Response.ok(userdet).build();
+
+		EntityTag entityTag = new EntityTag(userdet.getVersion().toString());
+
+		ResponseBuilder evaluatePreconditions = request.evaluatePreconditions(entityTag);
+		if (evaluatePreconditions != null)
+			return evaluatePreconditions.build();
+
+		return Response.ok(userdet).tag(entityTag).build();
 	}
 
 	@GET
 	@Produces("application/pdf")
 	public Response getUserDetailsPdf() throws IOException {
 		File pdf = new File("src/main/resources/present.pdf");
-		return Response.ok(pdf).header("Content-Disposition", "attachment; filename=Presentazione.pdf").build();
+
+		Date lastModified = new Date(pdf.lastModified());
+
+		ResponseBuilder evaluatePreconditions = request.evaluatePreconditions(lastModified);
+		if (evaluatePreconditions != null)
+			return evaluatePreconditions.build();
+
+		return Response.ok(pdf).header("Content-Disposition", "attachment; filename=Presentazione.pdf").lastModified(lastModified).build();
 	}
 
 	@XmlRootElement(name = "user")
@@ -47,6 +70,7 @@ public class Presentation {
 		private String email;
 		private String blog;
 		private String linkedin;
+		private Integer version;
 
 		public String getRole() {
 			return role;
@@ -86,6 +110,14 @@ public class Presentation {
 
 		public void setLinkedin(String linkedin) {
 			this.linkedin = linkedin;
+		}
+
+		public Integer getVersion() {
+			return version;
+		}
+
+		public void setVersion(Integer version) {
+			this.version = version;
 		}
 
 	}
